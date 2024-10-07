88851f917c468c671ec13ab6fbc8de6a8495c8b4
cen7e <alvarocentella@hotmail.com>
2024-10-07 16:20:11 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <!-- Aquí puedes integrar tu código actual -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Explora Ciudades"
+    };
+  }
+}
+</script>
+
+<style>
+/* Puedes agregar tus estilos aquí */
+</style>
